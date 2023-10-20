e6d16aee3006d3b533db39aaa98aa011bd9bbea9
Tinnaphop Choonchuachan <thathobotfy64@gmail.com>
2023-10-20 12:18:00 +0700

    change: used new fetch resource

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-const PROJECTS = [
-	{
-		text: "/",
-		projectURL: "https://content.mistertfy64.com",
-		repositoryURL: "https://github.com/mistertfy64/mistertfy64.github.io",
-		repositoryDataURL:
-			"https://api.github.com/repos/mistertfy64/mistertfy64.github.io/commits/master",
-	},
-	{
-		text: "/ThaiPrimeMinisterCounter",
-		projectURL: "https://content.mistertfy64.com/ThaiPrimeMinisterCounter",
-		repositoryURL:
-			"https://github.com/mistertfy64/ThaiPrimeMinisterCounter",
-		repositoryDataURL:
-			"https://api.github.com/repos/mistertfy64/ThaiPrimeMinisterCounter/commits/main",
-	},
-];
-
 // <a class="link--project">/ThaiPrimeMinisterCounter</a>&nbsp;<a class="link--commit"></a>
 
 async function initialize() {
-	for (const project of PROJECTS) {
-		// get data
-		const data = await (await fetch(project.repositoryDataURL)).json();
+	const data = await (
+		await fetch(
+			"http://api.mistertfy64.com/content-subdomain-project-entries"
+		)
+	).json();
+	for (const project of data) {
 		// create entry
 		const projectEntryElement = document.createElement("div");
 		projectEntryElement.classList = ["project-entry"];
@@ -32,15 +17,15 @@ async function initialize() {
 		projectLinkElement.href = project.projectURL;
 		projectEntryElement.appendChild(projectLinkElement);
 		// create commit hash link
-		const lastCommitHashText = data.sha.toString().substring(0, 7);
+		const lastCommitHashText = `${project.repositoryText}`;
 		const projectCommitElement = document.createElement("a");
 		projectCommitElement.text = lastCommitHashText;
 		projectCommitElement.classList = ["link--commit"];
 		projectCommitElement.href = project.repositoryURL;
 		// create date link
-		const lastCommitDataHashText = `${data.commit.author.date.toString()} ${data.author.login.toString()}`;
+		const lastCommitDataText = `${project.modifiedDateText}`;
 		const projectCommitDataElement = document.createElement("a");
-		projectCommitDataElement.text = lastCommitDataHashText;
+		projectCommitDataElement.text = lastCommitDataText;
 		projectCommitDataElement.classList = ["link--date"];
 		// projectCommitDataElement.href = project.repositoryURL;
 		// add smaller elements
